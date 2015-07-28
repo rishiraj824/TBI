5de132aaa45ceb642aafb2dfee76cf57a1201d20
Rishi Raj <rishiraj824@gmail.com>
2015-07-28 15:25:08 +0530

    Main website

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function()
+{
+	var app=angular
+	.module('tbi',[]);
+	app.controller('tbiController', function(){
+		this.tab = 1;
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
+	app.controller('navController', function(){
+		this.tab = 0;
+		this.selectTab = function(setTab) {
+			if(this.tab==setTab)
+				this.tab = 0;
+			else
+				this.tab=setTab;
+		};
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+})();
