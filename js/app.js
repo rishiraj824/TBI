8a48ff679f33fc589b1a7e5e57c3b4da430fa410
Rishi Raj <rishiraj824@gmail.com>
2015-08-04 23:27:08 +0530

    newpages

--- js/app.js
+++ js/app.js
@@ -24,5 +24,22 @@
 			return this.tab === checkTab;
 		};
 	});
+	
 
 })();
+/*app.config(function($stateProvider, $urlRouterProvider) {
+	    
+	    $urlRouterProvider.otherwise('/home');
+	    
+	    $stateProvider
+	        
+	        // HOME STATES AND NESTED VIEWS ========================================
+	        .state('home', {
+	            url: '/home',
+	            templateUrl: 'partial-home.html'
+	        })
+	        
+	        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+	        .state('about', {
+	            // we'll get to this in a bit       
+	});*/
